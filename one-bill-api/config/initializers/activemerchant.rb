7ad1dca51f2d7aa0b3f013d3168368556f777e97
Yaniv Pinchas <yanivps@gmail.com>
2018-04-26 23:18:29 +0300

    active_merchant patch to fix ssl error

--- one-bill-api/config/initializers/activemerchant.rb
+++ one-bill-api/config/initializers/activemerchant.rb
@@ -10,6 +10,8 @@ if Rails.env == "development"
   paypal_password = ENV['PAYPAL_PASSWORD_TEST']
   paypal_signature = ENV['PAYPAL_SIGNATURE_TEST']
 elsif Rails.env == "production"
+  # TODO: remove Base.mode = :test when working in real production environment
+  ActiveMerchant::Billing::Base.mode = :test
   login = ENV['FIRSTDATA_LOGIN']
   password = ENV['FIRSTDATA_PASSWORD']
 
@@ -29,3 +31,25 @@ PAYPAL_GATEWAY = ActiveMerchant::Billing::PaypalExpressGateway.new({
       password: paypal_password,
       signature: paypal_signature
 })
+
+
+
+
+# ActiveMerchant SSL Patch (For Heroku)
+module ActiveMerchant
+  class Connection
+    def configure_ssl(http)
+      return unless endpoint.scheme == "https"
+
+      http.use_ssl = true
+
+      if verify_peer
+        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        # Fix SSLv3 failure
+        # http.ca_file     = File.dirname(__FILE__) + '/../../certs/cacert.pem'
+      else
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
+    end
+  end
+end
